555fe047cf4710fa3015117007b6572bdc6473ea
Narimaan <narimaanvalian@Narimaans-MacBook-Pro.local>
2017-02-07 10:42:13 -0500

    update

--- AsteroidCowboys/js/main.js
+++ AsteroidCowboys/js/main.js
@@ -13,68 +13,22 @@ window.onload = function() {
     
     "use strict";
     
-    var game = new Phaser.Game( 1200, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 1200, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render} );
     
     function preload() {
-        game.load.image( 'asteroid', 'assets/cowboy.png' );
+        game.load.image( 'asteroid', 'assets/asteroid.png' );
         game.load.image( 'bullet', 'assets/bullet-s.png' );
-        game.load.image( 'cowboy', 'assets/cowboy-s.png' );
+        game.load.image( 'player', 'assets/cowboy-s.png' );
         game.load.image( 'background', 'assets/space.jpg' );
         game.load.image( 'earth', 'assets/earth.png' );
         game.load.image( 'logo', 'assets/phaser.png' );
     }
     
-    var bouncy;
     var speed = 6;
-    
-    var Bullet = function (game, key)
-    {
-
-        Phaser.Sprite.call(this, game, 0, 0, key);
-
-        this.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
-
-        this.anchor.set(0.5);
-
-        this.checkWorldBounds = true;
-        this.outOfBoundsKill = true;
-        this.exists = false;
-
-        this.tracking = false;
-        this.scaleSpeed = 0;
-
-    };
-    
-//    Weapon.SingleBullet = function (game)
-//    {
-//
-//        Phaser.Group.call(this, game, game.world, 'Single Bullet', false, true, Phaser.Physics.ARCADE);
-//
-//        this.nextFire = 0;
-//        this.bulletSpeed = 600;
-//        this.fireRate = 100;
-//
-//        for (var i = 0; i < 64; i++)
-//        {
-//            this.add(new Bullet(game, 'bullet'), true);
-//        }
-//
-//        return this;
-//
-//    };
-//    
-//    Weapon.SingleBullet.prototype.fire = function (source)
-//    {
-//        if (this.game.time.time < this.nextFire) { return; }
-//
-//        var x = source.x + 10;
-//        var y = source.y + 10;
-//
-//        this.getFirstExists(false).fire(x, y, 0, this.bulletSpeed, 0, 0);
-//
-//        this.nextFire = this.game.time.time + this.fireRate;
-//
-//    };
+    var bullet, bullets;
+    var asteroid, asteroids;
+    var bulletTime = 0;
+    var asteroidTime = 0;
     
     function create() {
         this.background = game.add.image( 0, 0, 'background');
@@ -84,32 +38,35 @@ window.onload = function() {
         this.earth.scale.setTo(0.5, 0.5);
         this.earth.angle = 25;
         
-        this.cowboy = game.add.sprite(game.world.centerX*0.5, game.world.centerY, 'cowboy');
-        this.cowboy.anchor.setTo(0.5, 0.5);
-        this.cowboy.scale.setTo(0.4);
-        game.physics.enable( this.cowboy, Phaser.Physics.ARCADE );
-        this.cowboy.body.collideWorldBounds = true;
-                
-        //this.weapons = [];
-        //this.weapons.push(new Weapon.SingleBullet(this.game));
+        this.player = game.add.sprite(game.world.centerX*0.5, game.world.centerY, 'player');
+        this.player.anchor.setTo(0.5, 0.5);
+        this.player.scale.setTo(0.4);
+        game.physics.enable( this.player, Phaser.Physics.ARCADE );
+        this.player.body.collideWorldBounds = true;
+        this.player.body.setSize(100, 100, 100, 100);
         
+        bullets = game.add.group();
+        bullets.enableBody = true;
+        bullets.physicsBodyType = Phaser.Physics.ARCADE;
+        bullets.createMultiple(30, 'bullet');
+        bullets.setAll('anchor.x', 1);
+        bullets.setAll('anchor.y', 0.5);
+        bullets.setAll('scale.x', -0.1);
+        bullets.setAll('scale.y', 0.1);
+        bullets.setAll('outOfBoundsKill', true);
+        bullets.setAll('checkWorldBounds', true);
         
-        // Create a sprite at the center of the screen using the 'logo' image.
-        //bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        //bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        //game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        //bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        //var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        //var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        //text.anchor.setTo( 0.5, 0.0 );
+        // The enemy's bullets
+        asteroids = game.add.group();
+        asteroids.enableBody = true;
+        asteroids.physicsBodyType = Phaser.Physics.ARCADE;
+        asteroids.createMultiple(30, 'asteroid');
+        asteroids.setAll('anchor.x', 1);
+        asteroids.setAll('anchor.y', 0.5);
+        asteroids.setAll('scale.x', 0.2);
+        asteroids.setAll('scale.y', 0.2);
+        asteroids.setAll('outOfBoundsKill', true);
+        asteroids.setAll('checkWorldBounds', true);
     }
     
     function update() {
@@ -126,29 +83,80 @@ window.onload = function() {
         
         if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
         {
-            this.cowboy.y -= speed;
+            this.player.y -= speed;
         }
         
         if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
         {
-            this.cowboy.y += speed;
+            this.player.y += speed;
         }
         
         if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
         {
-            this.bullet = game.add.sprite(this.cowboy.x + 100, this.cowboy.y, 'bullet');
-            this.bullet.anchor.setTo(1, 0.5);
-            this.bullet.scale.setTo(-0.15, 0.15);
-            
-            //game.physics.p2.enable(this.bullet);
-            //this.bullet.body.moveRight(500);
-           
-            //game.physics.enable(this.bullet, Phaser.Physics.ARCADE);
-//            game.physics.arcade.accelerateToXY(this.bullet, this.cowboy.x + 100 + 500, this.cowboy.y, 1000, 1000, 0);
-            //this.bullet.body.velocity()
-            //this.bullet.kill();
-            
-            //this.weapons[0].fire(this.cowboy);
+            fireBullet(this);
         }
+        
+        if (game.input.keyboard.isDown(Phaser.Keyboard.M))
+        {
+            fireAsteroid(this);
+        }
+        
+        game.physics.arcade.overlap(bullets, asteroids, bulletHitAsteroid, null, this);
     }
+    
+    function render()
+    {
+        game.debug.body(this.player);
+        game.debug.body(bullets);
+        game.debug.body(asteroids);
+        game.debug.body(this.earth);
+    }
+    
+    
+    function fireBullet (scope)
+    {
+
+        //  To avoid them being allowed to fire too fast we set a time limit
+        if (game.time.now > bulletTime)
+        {
+            //  Grab the first bullet we can from the pool
+            bullet = bullets.getFirstExists(false);
+
+            if (bullet)
+            {
+                //  And fire it
+                bullet.reset(scope.player.x + 80, scope.player.y);
+                bullet.body.velocity.x = 400;
+                bulletTime = game.time.now + 400;
+            }
+        }
+
+    }
+    
+    function fireAsteroid (scope)
+    {
+
+        //  To avoid them being allowed to fire too fast we set a time limit
+        if (game.time.now > asteroidTime)
+        {
+            //  Grab the first bullet we can from the pool
+            asteroid = asteroids.getFirstExists(false);
+
+            if (asteroid)
+            {
+                //  And fire it
+                asteroid.reset(1200, scope.player.y);
+                asteroid.body.velocity.x = -300;
+                asteroidTime = game.time.now + 400;
+            }
+        }
+
+    }
+    
+    function bulletHitAsteroid(bullet, asteroid)
+    {
+        bullet.kill();
+        asteroid.kill();
+    }
+    
 };
