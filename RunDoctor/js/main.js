c233e34481cf1e51974a580b8159da056d53e579
Narimaan <narimaanvalian@Narimaans-MacBook-Pro.local>
2017-02-15 21:21:08 -0500

    update

--- RunDoctor/js/main.js
+++ RunDoctor/js/main.js
@@ -13,20 +13,22 @@ window.onload = function() {
     
     "use strict";
     
-    var game = new Phaser.Game( 1000, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 1000, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render} );
     
     function preload()
     {
         game.load.atlasJSONHash('player', 'assets/playerAnim.png', 'assets/playerAnim.json');
         game.load.image('background', 'assets/background.png');
         game.load.image('button', 'assets/button.png');
+        game.load.image('dog','assets/dog.png');
+        game.load.image('hole', 'assets/hole.png');
     }
     
-    var player, background, button;
-    var upKey;
+    var player, background, button, dog, dogs, hole, holes;
     var runAnim, jumpAnim;
-    var runSpeed = 15, scrollSpeed = -300, jumpSpeed = 300;
-    var jumpTimer = 0;
+    var runSpeed = 15, scrollSpeed = -300, jumpSpeed = 250; //300
+    var jumpDelay = 0, enemyDelay = 0;
+    var enemyChoice;
     
     function create()
     {
@@ -34,7 +36,7 @@ window.onload = function() {
         game.physics.arcade.gravity.y = 250;
         
         background = game.add.tileSprite(0,0,1000,600,'background');
-        game.world.setBounds(0,0,1000,442);
+        game.world.setBounds(0, 0, 1000, 442);
         background.autoScroll(scrollSpeed, 0);
         
         
@@ -43,31 +45,127 @@ window.onload = function() {
         runAnim = player.animations.add('run', Phaser.Animation.generateFrameNames('run', 1, 8,'.png'), null, true);
         jumpAnim = player.animations.add('jump', ['jump1.png','jump2.png','jump3.png','jump4.png','jump1.png',], null);
         player.animations.play('run', runSpeed);
-        
         game.physics.enable(player, Phaser.Physics.ARCADE);
         player.body.collideWorldBounds = true;
+
+        dogs = game.add.group();
+        dogs.enableBody = true;
+        dogs.physicsBodyType = Phaser.Physics.ARCADE;
+        dogs.createMultiple(5, 'dog');
+        dogs.setAll('anchor.x', 0.5);
+        dogs.setAll('anchor.y', 1);
+        dogs.setAll('outOfBoundsKill', true);
+        dogs.setAll('checkWorldBounds', true);
         
-        button = game.add.button(500, 520, 'button', function(){button.tint = 0x808080; jump();});
+        holes = game.add.group();
+        holes.enableBody = true;
+        holes.physicsBodyType = Phaser.Physics.ARCADE;
+        holes.createMultiple(5, 'hole');
+        holes.setAll('anchor.x', 0.5);
+        holes.setAll('anchor.y', 0);
+        holes.setAll('outOfBoundsKill', true);
+        holes.setAll('checkWorldBounds', true);
+        holes.setAll('alpha', 0.75);
+        
+        button = game.add.button(500, 550, 'button', jump);
         button.anchor.setTo(0.5, 0.5);
-        
-        upKey = game.input.keyboard.addKey(Phaser.KeyCode.UP);
-        game.input.keyboard.addKeyCapture(upKey);
-        upKey.onDown.add(jump);
     }
     
     function update()
     {
+        enemyChoice = Math.floor(Math.random() * 2);
         
+        if (enemyChoice === 0)
+        {
+            spawnDog();
+        }
+        
+        else
+        {
+            spawnHole();
+        }
+        
+        if (game.input.keyboard.isDown(Phaser.Keyboard.ENTER))
+        {
+            game.state.start(game.state.current);
+        }
+        
+        game.physics.arcade.overlap(player, dogs, endGameWin, null, this);
+        game.physics.arcade.overlap(player, holes, endGameWin, null, this);
+    }
+    
+    function render()
+    {
+        //game.debug.body(player);
+//        dogs.forEachAlive(renderGroup, this);
+//        holes.forEachAlive(renderGroup, this);
+    }
+    
+    function renderGroup(member)
+    {
+        game.debug.body(member);
     }
     
     function jump()
     {
-        if (game.time.now > jumpTimer)
+        if (game.time.now > jumpDelay)
         {
-            jumpTimer = game.time.now + 2500;
+            button.tint = 0x808080;
+            jumpDelay = game.time.now + 2000; // 2500
             player.body.velocity.y -= jumpSpeed;
-            player.animations.play('jump', 2.1);
+            player.animations.play('jump', 5/2); // 2.1
             jumpAnim.onComplete.add(function() {player.animations.stop('jump'); player.animations.play('run', runSpeed); button.tint = 0xFFFFFF;});
         }
+    }
+    
+    function spawnDog()
+    {
+        if (game.time.now > enemyDelay)
+        {
+            
+            dog = dogs.getFirstExists(false);
+            
+            if (dog)
+            {
+                dog.reset(1000, 450);
+                dog.body.acceleration.y = -250;
+                dog.body.velocity.x = -600;
+                enemyDelay = game.time.now + Math.floor((Math.random()*3)+3)*1000;
+            }
+        }
+    }
+    
+    function spawnHole()
+    {
+        if (game.time.now > enemyDelay)
+        {
+            
+            hole = holes.getFirstExists(false);
+            
+            if (hole)
+            {
+                hole.reset(1000, 442);
+                hole.body.setSize(146, 1, -2, -1);
+                hole.body.acceleration.y = -250;
+                hole.body.velocity.x = -300;
+                enemyDelay = game.time.now + Math.floor((Math.random()*3)+3)*1000;
+            }
+        }
+    }
+    
+    function endGameLoss()
+    {
+        player.destroy();
+        holes.destroy();
+        dogs.destroy();
+        background.stopScroll();
+    }
+    
+    function endGameWin()
+    {
+        holes.destroy();
+        dogs.destroy();
+        background.stopScroll();
+        player.body.velocity.x = 500;
     }
 };
