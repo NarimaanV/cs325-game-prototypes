c2efc3421f797ab13de244371d0ddd88e44e67b1
Narimaan Valian <Narimaan Valian>
2017-04-05 22:41:35 -0400

    update

--- digitalGame7/js/Game.js
+++ digitalGame7/js/Game.js
@@ -24,12 +24,12 @@ BasicGame.Game = function (game) {
     //  But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
     
     // Create your own variables.
-    this.bouncy = null;
     this.player = null;
     this.background = null;
-    this.speed = 15;
+    this.speed = 4;
     this.enemies = null, this.enemy = null;
     this.delay = 0;
+    this.score = 0;
 };
 
 BasicGame.Game.prototype = {
@@ -40,10 +40,9 @@ BasicGame.Game.prototype = {
         this.game.world.setBounds(0, 0, 768, 512);
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.array = ['rock', 'paper', 'scissors'];
-        this.player = this.game.add.sprite(768/2, 128*3, 'rock');
+        this.player = this.game.add.sprite(768/2, 128*3, this.array[Math.floor(Math.random() * 3)]);
         this.player.anchor.setTo(0.5, 0.5);
-
-        this.spaceKey = this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+        
         this.game.physics.arcade.enable(this.player);
         this.player.body.collideWorldBounds = true;
         this.game.camera.follow(this.player);
@@ -58,20 +57,49 @@ BasicGame.Game.prototype = {
         this.input.keyboard.addKeyCapture(this.leftKey);
         this.input.keyboard.addKeyCapture(this.rightKey);
         this.input.keyboard.addKeyCapture(this.spaceKey);
-        
-        this.bouncy = this.game.add.sprite(768/2, -128, 'paper');
-        this.bouncy.anchor.setTo(0.5, 0.5);
-        this.game.physics.arcade.enable(this.bouncy);
-        this.bouncy.body.velocity.x = -500;
-        
+
         this.enemies = this.game.add.group();
         this.enemies.enableBody = true;
         this.enemies.physicsBodyType = Phaser.Physics.ARCADE;
         this.enemies.createMultiple(30 , 'rock');
         this.enemies.setAll('anchor.x', 0.5);
         this.enemies.setAll('anchor.y', 0.5);
-        this.enemies.setAll('outOfBoundsKill', true);
-        this.enemies.setAll('checkWorldBounds', true);
+        
+        for (var i = 0; i < 6; i++)
+        {
+            this.enemy = this.enemies.getFirstExists(false);
+            
+            if (this.enemy)
+            {
+                this.enemy.reset(768 - i*140, 128);
+                this.enemy.body.velocity.x = -50;
+                this.change(this.enemy, this.array[Math.floor(Math.random() * 3)]);
+            }
+        }
+        
+        for (var i = 0; i < 6; i++)
+        {
+            this.enemy = this.enemies.getFirstExists(false);
+            
+            if (this.enemy)
+            {
+                this.enemy.reset(768 - i*140, -128);
+                this.enemy.body.velocity.x = -50;
+                this.change(this.enemy, this.array[Math.floor(Math.random() * 3)]);
+            }
+        }
+        
+//        for (var i = 0; i < 6; i++)
+//        {
+//            this.enemy = this.enemies.getFirstExists(false);
+//            
+//            if (this.enemy)
+//            {
+//                this.enemy.reset(768 - i*140, -384);
+//                this.enemy.body.velocity.x = -50;
+//                this.change(this.enemy, this.array[Math.floor(Math.random() * 3)]);
+//            }
+//        }
     },
 
     update: function () {
@@ -95,52 +123,107 @@ BasicGame.Game.prototype = {
             this.player.position.x += this.speed;
         }
         
-        if (this.bouncy.y >= 512 + 50)
-        {
-            this.bouncy.kill();
-        }
+        this.enemies.forEachAlive(this.enemyHandler, this);
         
-        if (this.bouncy.x <= -36.5)
-        {
-            this.bouncy.x = 768 + 36.5;
-        }
-        
-        if (this.spaceKey.isDown && this.game.time.now > this.delay)
-        {
-            this.change(this.player, this.array[Math.floor(Math.random() * 3)]);
-            this.delay = this.game.time.now + 250;
-        }
+       // this.game.physics.arcade.overlap(this.player, this.enemies, this.collisionHandler, null, this);
     },
     
-    render: function() {
-        this.game.debug.body(this.player);
-        this.game.debug.body(this.bouncy);
+//    render: function() {
+//        this.game.debug.body(this.player);
+//        this.enemies.forEachAlive(this.renderGroup, this);
+//    },
+    
+    renderGroup: function(enemy) {
+        this.game.debug.body(enemy);
+    },
+    
+    enemyHandler: function(enemy) {
+        if (enemy.x <= -36.5)
+        {
+            enemy.x = 768 + 36.5;
+        }
+        
+        if (enemy.y >= 512 + 50)
+        {
+            enemy.kill();
+        }
     },
 
-    quitGame: function (pointer) {
-
-        //  Here you should destroy anything you no longer need.
-        //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
-
-        //  Then let's go back to the main menu.
+    quitGame: function(pointer) {
+        this.player.destroy();
+        this.enemies.destroy();
+        this.background.destroy();
         this.state.start('MainMenu');
 
     },
     
+    collisionHandler: function(player, enemy) {
+        if (player.key === enemy.key)
+        {
+            return;    
+        }
+        
+        else if (player.key === 'rock' && enemy.key === 'paper')
+        {
+            this.quitGame();
+        }
+        
+        else if (player.key === 'paper' && enemy.key === 'rock')
+        {
+            enemy.kill();
+            this.change(player, 'rock');
+        }
+        
+        else if (player.key === 'scissors' && enemy.key === 'rock')
+        {
+            this.quitGame();
+        }
+        
+        else if (player.key === 'rock' && enemy.key === 'scissors')
+        {
+            enemy.kill();
+            this.change(player, 'scissors');
+        }
+        
+        else if (player.key === 'paper' && enemy.key === 'scissors')
+        {
+            this.quitGame();
+        }
+        
+        else if (player.key === 'scissors' && enemy.key === 'paper')
+        {
+            enemy.kill();
+            this.change(player, 'paper');
+        }
+    },
+    
     change: function(object, name) {
-        this.width = this.game.cache.getImage(name).width;
-        this.height = this.game.cache.getImage(name).height;
         object.loadTexture(name);
-        object.body.setSize(this.width, this.height, 0, 0)
+        object.body.setSize(75, 100, 0, 0);
     },
     
     movePlayerUp: function() {
         this.background.tilePosition.y += this.speed;
-        this.bouncy.position.y += this.speed;
+        if (this.background.tilePosition.y % 256 === 128 && this.game.time.now > this.delay)
+        {
+            for (var i = 0; i < 6; i++)
+            {
+                this.enemy = this.enemies.getFirstExists(false);
+
+                if (this.enemy)
+                {
+                    this.enemy.reset(768 - i*140, -260);
+                    this.enemy.body.velocity.x = -50;
+                    this.change(this.enemy, this.array[Math.floor(Math.random() * 3)]);
+                }
+            }
+            this.delay = this.game.time.now + 1000;
+        }
+        this.enemies.forEachAlive(function(enemy) {enemy.position.y += this.speed;}, this);
     },
 
     movePlayerDown: function() {
         this.background.tilePosition.y -= this.speed;
-        this.bouncy.position.y -= this.speed;
+        this.enemies.forEachAlive(function(enemy) {enemy.position.y -= this.speed;}, this);
     }
 };
